8577f484fba90440f01b3498ac2bf5f90bcf8f84
mbryniak <m@m>
2023-11-04 15:16:23 +0100

    Dublujemy

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("sadfsdafsd");
+        System.out.println("sadfsdafsd");
+        System.out.println("sadfsdafsd");
+        System.out.println("sadfsdafsd");
+        System.out.println("sadfsdafsd");
+        System.out.println("sadfsdafsd");
+        System.out.println("sadfsdafsd");
+
     }
 }
