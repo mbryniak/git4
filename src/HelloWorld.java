2e0d82d469a7d7226716fd9c397899a25397dada
mbryniak <m@m>
2023-11-04 15:16:38 +0100

    Revert "Dublujemy"
    This reverts commit 8577f484fba90440f01b3498ac2bf5f90bcf8f84.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("sadfsdafsd");
-        System.out.println("sadfsdafsd");
-        System.out.println("sadfsdafsd");
-        System.out.println("sadfsdafsd");
-        System.out.println("sadfsdafsd");
-        System.out.println("sadfsdafsd");
-        System.out.println("sadfsdafsd");
-
     }
 }
